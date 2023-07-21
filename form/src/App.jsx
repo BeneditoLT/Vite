fc2d7405ae0b547af309f8edb1130f85edfacf91
Lucas <benedito.lucas@gmail.com>
2023-07-20 22:28:21 -0300

    form

---
+++ form/src/App.jsx
@@ -0,0 +1,17 @@
+import BasicForm from './componets/BasicForm'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h2>Eletrokit</h2>
+      <BasicForm/>
+
+    </div>
+
+  )
+}
+
+export default App
